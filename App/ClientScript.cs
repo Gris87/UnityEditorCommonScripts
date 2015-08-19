2f6ee895e83846adc54e55b7b24f0fe1dad697b7
Gris87 <Gris87@yandex.ru>
2015-08-19 12:36:05 +0300

    Assets refactoring

---
+++ App/ClientScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+
+namespace Common.App
+{
+	/// <summary>
+	/// Client script.
+	/// </summary>
+	public class ClientScript : MonoBehaviour
+	{
+		/// <summary>
+		/// Script starting callback.
+		/// </summary>
+		void Start()
+		{
+			// TODO: [Major] Implement ClientScript
+		}
+	}
+}
